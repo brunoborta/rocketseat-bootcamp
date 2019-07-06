a129c34942798806254f0169bb15ec761e2aefb0
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-07-06 14:22:05 -0300

    Inicializando o mongodb

---
+++ modulo02e03/src/app/controllers/AppointmentController.js
@@ -0,0 +1,94 @@
+import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      provider_id: yup.number().required(),
+      date: yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation fails',
+      });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({
+        error: 'You can only create appointments if you are a provider',
+      });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' });
+    }
+
+    const isNotAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (isNotAvailable) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available.' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+
+  async index(req, res) {
+    const LIMIT_PER_PAGE = 20;
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+      // Pagination
+      limit: LIMIT_PER_PAGE,
+      offset: (page - 1) * LIMIT_PER_PAGE,
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new AppointmentController();
--- modulo02e03/src/routes.js
+++ modulo02e03/src/routes.js
@@ -1,16 +1,32 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// A partir daqui, todas as rodas necessitam do token de autenticação
 routes.use(authMiddleware);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 
 routes.put('/users', UserController.update);
 
