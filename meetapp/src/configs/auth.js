d680916997d3800582e8eef6a1aebfc23e3e4882
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-07-08 17:25:44 -0300

    Transformando Meetapp um projeto separado

--- meetapp/src/configs/auth.js
+++
@@ -1,1 +0,0 @@
-export default { secret: 'c9fe27dbd07e4717881a89a9261a74ef', expiresIn: '7d' };
--- meetapp/src/server.js
+++
@@ -1,3 +0,0 @@
-import server from './app';
-
-server.listen(3333, () => console.log('Ouvindo na porta', 3333));
