2b064aed2be00d7c3bc436304daf2c400895a6a5
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-07-15 19:07:31 -0300

    Adicionado modulo 05 e criado styled components para a main page. WIP: Listar repos

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton } from './styles';
+
+class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = event => {
+    this.setState({ newRepo: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { newRepo, repositories } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={newRepo}
+            placeholder="Adicionar repositório"
+            name="repo"
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default Main;
