2fe51ee32db290dc1d47c8436d8555200a25fea3
Bruno "Borta" Bortagaray <bruno.bortagaray@objectedge.com>
2019-08-12 19:05:25 -0300

    Adicionado o reduxPersist e loading

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
-import { signInRequest } from '~/store/modules/auth/actions';
-
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  const dispatch = useDispatch();
-  function handleSubmit({ email, password }) {
-    dispatch(signInRequest(email, password));
-  }
-
   const schema = Yup.object().shape({
     name: Yup.string().required('O nome é obrigatório'),
     email: Yup.string()
@@ -28,7 +20,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="" />
 
-      <Form schema={schema} onSubmit={handleSubmit}>
+      <Form schema={schema}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <Input
