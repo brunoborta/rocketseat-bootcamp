da29c6729796a2ff9f22998c6b0965b90c9a62ad
Bruno "Borta" Bortagaray <bruno.bortagaray@objectedge.com>
2019-08-13 11:56:22 -0300

    Terminada autenticacao. Adicionado header e notificacoes

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 export default function SignUp() {
   const schema = Yup.object().shape({
@@ -16,11 +19,17 @@ export default function SignUp() {
       .required('A senha é obrigatória'),
   });
 
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="" />
 
-      <Form schema={schema}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <Input
